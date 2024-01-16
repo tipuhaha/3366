bc3bf3038569a5833b52bc153716ad1ca1bcd144
tipuhaha <156343470+tipuhaha@users.noreply.github.com>
2024-01-16 22:03:47 +0500

    Add files via upload

---
+++ johar lab.py
@@ -0,0 +1,17 @@
+#question 1
+
+#user numbers
+
+num1= int(input('enter number 1'))
+num2= int(input('enter number 2'))
+num3= int(input('enter number 3'))
+num4= int(input('enter number 4'))
+num5= int(input('enter number 5'))
+
+#sum
+sum= (num1+num2+num3+num4+num5)
+
+#printing
+print("the sum is:" ,sum)
+
+#done
