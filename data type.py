f0d1f1dd12ec50f5b22aa978afe48328ee7a9b2c
tipuhaha <156343470+tipuhaha@users.noreply.github.com>
2024-01-16 22:01:08 +0500

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,30 @@
+#data tyoes of python
+
+#integer
+num1= 5
+print(num1,"types is: ",type (num1))
+
+#float
+num2=2.2
+print(num2,"type is:" ,type(num2))
+
+#string
+word="tipu"
+print(word,"types is: ",type(word))
+      
+#boolean
+Booleans=(5==5)
+print(Booleans,"types is: ", type(Booleans))
+#list
+hehe=(33,"aa")
+print(hehe, 'types is:' ,type(hehe))
+#set
+haha={33,"hh"}
+print(haha,"types is:" , type(haha))
+
+#tuple
+hghg=["pp","kk"]
+print(hghg,"types is:" , type(hghg))
+#dictionary
+tipu={"jaan":"life"}
+print(tipu,"types is:" , type(tipu))
